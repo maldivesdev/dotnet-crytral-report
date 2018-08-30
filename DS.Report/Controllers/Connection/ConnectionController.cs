f35e8b8cee1c2212cfc909c8fb4af6fefea83bbd
nuttaphonratsameeraweemat <nuttaphon.rat@gmail.com>
2018-08-30 17:05:20 +0700

    Update -Controller for test connection

---
+++ DS.Report/Controllers/Connection/ConnectionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DS.Report.Controllers.Connection
+{
+    public class ConnectionController : Controller
+    {
+        // GET: Connection
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
